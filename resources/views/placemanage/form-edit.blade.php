2af2ebefe1958ec247652ba632b54a564d722e9a
YA <YA-androidapp@users.noreply.github.com>
2018-08-02 12:42:09 +0900

    add ids

--- resources/views/placemanage/form-edit.blade.php
+++ resources/views/placemanage/form-edit.blade.php
@@ -3,10 +3,10 @@
     <input type="hidden" name="id" value="{{$form->id}}">
     {{ method_field('PUT') }}
     {{ csrf_field() }}
-    <tr><th>desc: </th><td><input type="text" name="desc" value="{{$form->desc}}">@if($errors->has('desc')){{implode($errors->get('desc'))}}@endif</td></tr>
-    <tr><th>owner: </th><td><input type="text" name="owner" value="{{$form->owner}}">@if($errors->has('owner')){{implode($errors->get('owner'))}}@endif</td></tr>
-    <tr><th>lat: </th><td><input type="number" name="lat" value="{{$form->lat}}" step="0.000001">@if($errors->has('lat')){{implode($errors->get('lat'))}}@endif</td></tr>
-    <tr><th>lng: </th><td><input type="number" name="lng" value="{{$form->lng}}" step="0.000001">@if($errors->has('lng')){{implode($errors->get('lng'))}}@endif</td></tr>
+    <tr><th>desc: </th><td><input type="text" id="desc" name="desc" value="{{$form->desc}}">@if($errors->has('desc')){{implode($errors->get('desc'))}}@endif</td></tr>
+    <tr><th>owner: </th><td><input type="text" id="owner" name="owner" value="{{$form->owner}}">@if($errors->has('owner')){{implode($errors->get('owner'))}}@endif</td></tr>
+    <tr><th>lat: </th><td><input type="number" id="lat" name="lat" value="{{$form->lat}}" step="0.000001">@if($errors->has('lat')){{implode($errors->get('lat'))}}@endif</td></tr>
+    <tr><th>lng: </th><td><input type="number" id="lng" name="lng" value="{{$form->lng}}" step="0.000001">@if($errors->has('lng')){{implode($errors->get('lng'))}}@endif</td></tr>
     <tr><th></th><td><input type="submit" value="Update"></td></tr>
   </form>
 
--- resources/views/placemanage/form-show.blade.php
+++ resources/views/placemanage/form-show.blade.php
@@ -1,9 +1,9 @@
 <table>
   <form>
     {{ csrf_field() }}
-    <tr><th>desc: </th><td><input type="text" name="desc" value="{{$form->desc}}" readonly="readonly" disabled="disabled"></td></tr>
-    <tr><th>owner: </th><td><input type="text" name="owner" value="{{$form->owner}}" readonly="readonly" disabled="disabled"></td></tr>
-    <tr><th>lat: </th><td><input type="number" name="lat" value="{{$form->lat}}" step="0.000001" readonly="readonly" disabled="disabled"></td></tr>
-    <tr><th>lng: </th><td><input type="number" name="lng" value="{{$form->lng}}" step="0.000001" readonly="readonly" disabled="disabled"></td></tr>
+    <tr><th>desc: </th><td><input type="text" id="desc" name="desc" value="{{$form->desc}}" readonly="readonly" disabled="disabled"></td></tr>
+    <tr><th>owner: </th><td><input type="text" id="owner" name="owner" value="{{$form->owner}}" readonly="readonly" disabled="disabled"></td></tr>
+    <tr><th>lat: </th><td><input type="number" id="lat" name="lat" value="{{$form->lat}}" step="0.000001" readonly="readonly" disabled="disabled"></td></tr>
+    <tr><th>lng: </th><td><input type="number" id="lng" name="lng" value="{{$form->lng}}" step="0.000001" readonly="readonly" disabled="disabled"></td></tr>
   </form>
 </table>
