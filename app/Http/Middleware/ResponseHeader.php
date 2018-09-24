a107a396ff6e4ec1d59749ffe4de6c831a426470
YA <YA-androidapp@users.noreply.github.com>
2018-09-24 09:53:52 +0900

    security update

---
+++ app/Http/Middleware/ResponseHeader.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ResponseHeader
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->header('X-Frame-Options', 'DENY');
+        $response->header('X-XSS-Protection', '1; mode=block');
+        $response->header('X-Content-Type-Options', 'nosniff');
+        return $response;
+    }
+}
